31d49dc274e4de1bd5d9ae2d9f2a00b59be9b206
Francisco Douglas <98854868+franciscodouglas@users.noreply.github.com>
2025-09-05 10:34:57 -0300

    Aula 04 - Vetor em Java com POO

---
+++ aula04_ed_poo/src/Aluno.java
@@ -0,0 +1,26 @@
+public class Aluno {
+    private int ra;
+    private String nome;
+
+    public Aluno (int ra, String nome){
+        this.ra=ra;
+        this.nome=nome;
+    }
+
+    public void setRa(int ra){
+        this.ra=ra;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    public int getRa(){
+        return this.ra;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+
+}
